da5721612407b88d1f249895c49575e38eacea75
yisus82 <djalma8@gmail.com>
2023-03-01 10:34:22 +0100

    Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Logo from '$lib/assets/logo.svg';
+</script>
+
+<header>
+	<a href="/"><img src={Logo} alt="App logo" /></a>
+</header>
+
+<style>
+	img {
+		display: block;
+		width: 10vw;
+		margin: 1rem auto;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
+	import Header from '../components/Header.svelte';
 	import '../global.css';
 </script>
 
 <svelte:head>
 	<title>Movies Database</title>
 </svelte:head>
+
+<Header />
 
 <slot />
